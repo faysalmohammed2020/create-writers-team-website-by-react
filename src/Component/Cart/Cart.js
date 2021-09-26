995278d5915394fc2335bd51a42239ac2e9b56f3
Your Name <you@example.com>
2021-09-26 19:57:29 +0600

    Website Updated

--- src/Component/Cart/Cart.js
+++ src/Component/Cart/Cart.js
@@ -1,5 +1,7 @@
 import React from 'react';
-
+import'./Cart.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faShoppingCart,faMale } from '@fortawesome/free-solid-svg-icons';
 const Cart = (props) => {
     const{cart} = props;
     let total =0;
@@ -12,10 +14,9 @@ const Cart = (props) => {
         
         <div className="card" style={{ width: '18rem' }}>
   <div className="card-body">
-    <h5 className="card-title">Cart</h5>
+    <h5 className="card-title"><FontAwesomeIcon icon={faMale} /> Cart</h5>
     <h6 className="card-subtitle mb-2 text-muted">Writers Added: {props.cart.length}</h6>
-    {/* <p className="card-text">Added Persons Name: {props.cart.name}</p> */}
-    <p className="card-text">Total: {total}</p>
+    <p className="card-text"><FontAwesomeIcon icon={faShoppingCart}/> Total: {total}</p>
     <button className="btn btn-primary">Buy Now</button>
    
   </div>
