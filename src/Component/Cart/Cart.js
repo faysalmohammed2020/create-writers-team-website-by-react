5cfed348e78f4badc700937b1d0de3621b83e505
Your Name <you@example.com>
2021-09-26 21:22:20 +0600

    website final update

--- src/Component/Cart/Cart.js
+++ src/Component/Cart/Cart.js
@@ -9,14 +9,23 @@ const Cart = (props) => {
         total = total+member.salary;
         
     }
+    let name =" ";
+    for(const member of cart){
+        name = name + '      '+ member.name;
+    }
     
     return (
         
         <div className="card" style={{ width: '18rem' }}>
   <div className="card-body">
-    <h5 className="card-title"><FontAwesomeIcon icon={faMale} /> Cart</h5>
+    <h5 className="card-title"> Cart</h5>
     <h6 className="card-subtitle mb-2 text-muted">Writers Added: {props.cart.length}</h6>
     <p className="card-text"><FontAwesomeIcon icon={faShoppingCart}/> Total: {total}</p>
+    
+    <p className="cart-writer">  
+    <FontAwesomeIcon icon={faMale} /> Added Writer: <br/> {name}
+    </p>
+    
     <button className="btn btn-primary">Buy Now</button>
    
   </div>
